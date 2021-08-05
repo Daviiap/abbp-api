d40dc44404957abb19e027b310152167d08cd655
Davi Andrade Pontes <daviiap@outlook.com>
2021-08-05 19:42:07 -0300

    Atualizadno

--- commit.sh
+++ commit.sh
@@ -1,3 +1,9 @@
-git add .
-git commit -m "$1"
-git push
+currentBranch=$(git branch --show-current)
+
+if [ $currentBranch = "main" ]; then
+  echo "Você não pode usar esse comando na branch main!"
+else
+  git add .
+  git commit -m $1
+  git push --set-upstream origin $currentBranch
+fi
